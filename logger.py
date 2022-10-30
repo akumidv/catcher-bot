ab91441b27e77a8a5e252ec0b982e12e6cfa15d8
akumidv <akuminov@iondv.com>
2022-10-30 11:35:51 +1000

    formatter and split stderr and stdout for console

--- logger.py
+++ logger.py
@@ -5,39 +5,23 @@ import os
 import sys
 
 LOG_NAME = 'bot'
-FORMAT_STR = '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
+FORMAT_STR = '%(asctime)s [%(levelname)-7s] %(name)15s: %(message)s'
+FORMAT_STR_ERROR = '%(asctime)s [%(levelname)-7s] %(name)12s:%(lineno)-3d: %(message)s'
 DATE_FORMAT_STR = '%Y-%m-%d %H:%M:%S'
 HANDLERS = []
+MIN_LEVEL = logging.INFO
 
 
 
 def init(levels: Optional[Dict] = None):
     global HANDLERS
+    global MIN_LEVEL
     HANDLERS = list()
-    HANDLERS.extend(_get_handlers(levels))
+    handlers, set_level = _get_handlers_and_min_level(levels)
+    HANDLERS.extend(handlers)
+    if set_level is not None:
+        MIN_LEVEL = set_level
 
-    # console_handler = logging.StreamHandler(sys.stderr)
-    # console_handler.setLevel(log_level)
-    # HANDLERS.append(console_handler)
-    # class LessThanFilter(logging.Filter):
-    #     def __init__(self, exclusive_maximum, name=""):
-    #         super(LessThanFilter, self).__init__(name)
-    #         self.max_level = exclusive_maximum
-    #
-    #     def filter(self, record):
-    #         # non-zero return means we log this message
-    #         return 1 if record.levelno < self.max_level else 0
-    # base_level = get_logger_type()
-    # formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s')
-    # logging_handler_out = logging.StreamHandler(sys.stdout)
-    # logging_handler_out.setLevel(logging.DEBUG)
-    # logging_handler_out.addFilter(LessThanFilter(logging.WARNING))
-    # logging_handler_out.setFormatter(formatter)
-    # root_logger.addHandler(logging_handler_out)
-    # logging_handler_err = logging.StreamHandler(sys.stderr)
-    # logging_handler_err.setLevel(logging.WARNING)
-    # logging_handler_err.setFormatter(formatter)
-    # root_logger.addHandler(logging_handler_err)
 
 
 def get_logger(module_name: Optional[str] = None):
@@ -46,7 +30,7 @@ def get_logger(module_name: Optional[str] = None):
     name = LOG_NAME
     if module_name is not None:
         name = f'{LOG_NAME}:{module_name}'
-    logging.basicConfig(handlers=HANDLERS)
+    logging.basicConfig(handlers=HANDLERS, level=MIN_LEVEL)
     module_logger = logging.getLogger(name)
     module_logger.handlers = HANDLERS
 
@@ -69,30 +53,73 @@ def _get_logger_type(log_level=None, default='info'):
     return logging.INFO
 
 
-def _get_log_formatter() -> logging.Formatter:
-    return logging.Formatter(fmt=FORMAT_STR, datefmt=DATE_FORMAT_STR)
+def _get_log_formatter(fmt=FORMAT_STR_ERROR) -> logging.Formatter:
+    return logging.Formatter(fmt=fmt, datefmt=DATE_FORMAT_STR)
 
 
-def _get_handlers(levels: Optional[Dict] = None):
+def _get_handlers_and_min_level(levels: Optional[Dict] = None):
+    class LessOrMoreThanFilter(logging.Filter):
+        def __init__(self, level, should_less=True, name=""):
+            super(LessOrMoreThanFilter, self).__init__(name)
+            self.level = level
+            self.should_less = should_less
+
+        def filter(self, record): # non-zero return means we log this message
+            if self.should_less:
+                return 1 if record.levelno < self.level else 0
+            else:
+                return 1 if record.levelno >= self.level else 0
 
     handlers = []
     if levels is None or not isinstance(levels, dict):
         levels = {}
     cur_date = datetime.datetime.now()
     log_level = _get_logger_type(levels.get('console'), default=os.environ.get('LOG_LEVEL', 'info').lower())
-    console_handler = logging.StreamHandler(sys.stdout)
-    console_handler.setLevel(log_level)
-    console_handler.setFormatter(_get_log_formatter())
-    handlers.append(console_handler)
+    set_level = log_level
+
+    console_err_handler = logging.StreamHandler(sys.stderr)
+    console_err_handler.setLevel(log_level)
+    console_err_handler.setFormatter(_get_log_formatter())
+    handlers.append(console_err_handler)
+    if log_level < logging.WARNING:
+        console_err_handler.addFilter(LessOrMoreThanFilter(logging.WARNING, False))
+        console_handler = logging.StreamHandler(sys.stdout)
+        console_handler.setLevel(log_level)
+        console_handler.setFormatter(_get_log_formatter(FORMAT_STR_ERROR))
+        console_handler.addFilter(LessOrMoreThanFilter(logging.WARNING))
+        handlers.append(console_handler)
+
 
     if 'file' in levels:
         file_handler = logging.FileHandler(filename=f'bot_{cur_date.isoformat(timespec="minutes")}.log', mode='w',
                                            delay=True)  # RotatingFileHandler or TimedRotatingFileHandler may be better?
         log_level = _get_logger_type(levels['file'], default='debug')
+        if set_level > log_level:
+            set_level = log_level
         file_handler.setLevel(log_level)
-        file_handler.setFormatter(_get_log_formatter())
+        file_handler.setFormatter(_get_log_formatter(FORMAT_STR_ERROR))
         handlers.append(file_handler)
     if 'messanger' in levels:
         raise NotImplementedError('Messanger as log did not implemented yet')
-    return handlers
+    return handlers, set_level
 
+
+    # class LessThanFilter(logging.Filter):
+    #     def __init__(self, exclusive_maximum, name=""):
+    #         super(LessThanFilter, self).__init__(name)
+    #         self.max_level = exclusive_maximum
+    #
+    #     def filter(self, record):
+    #         # non-zero return means we log this message
+    #         return 1 if record.levelno < self.max_level else 0
+    # base_level = get_logger_type()
+    # formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s')
+    # logging_handler_out = logging.StreamHandler(sys.stdout)
+    # logging_handler_out.setLevel(logging.DEBUG)
+    # logging_handler_out.addFilter(LessThanFilter(logging.WARNING))
+    # logging_handler_out.setFormatter(formatter)
+    # root_logger.addHandler(logging_handler_out)
+    # logging_handler_err = logging.StreamHandler(sys.stderr)
+    # logging_handler_err.setLevel(logging.WARNING)
+    # logging_handler_err.setFormatter(formatter)
+    # root_logger.addHandler(logging_handler_err)
--- tests/logger_test.py
+++ tests/logger_test.py
@@ -8,31 +8,37 @@ import logging
 
 
 def test_get_logger():
-    logger.init({'file': 'debug'})
+    logger.init({'file': 'debug', 'console': 'info'})
     test_phrase = ['value', 'qwerty']
     for idx in range(2):
         log = logger.get_logger(module_name=f'test_{idx}')
         log.debug(f'{test_phrase[0]}_{idx}')
         log.warn(f'{test_phrase[1]}_{idx}')
-    log_fn = repr(log.handlers[1]).replace('<FileHandler ', '').replace(' (DEBUG)>', '')
+    file_handler_idx = [i for i in range(len(log.handlers)) if repr(log.handlers[i]).startswith('<FileHandler')]
+    assert len(file_handler_idx)
+    log_fn = repr(log.handlers[file_handler_idx[0]]).replace('<FileHandler ', '')
+    log_fn = log_fn[:log_fn.find('.log') + 4]
     with open(log_fn, 'r') as fl:
         file_contents = fl.read()
     os.remove(log_fn)
     print('Log file', log_fn, 'contain:')
     print(file_contents)
-    assert '[DEBUG]' in file_contents and '[WARNING]' in file_contents
+    assert '[DEBUG' in file_contents and '[WARNING' in file_contents
     assert 'test_0' in file_contents and 'value_0' in file_contents
     assert 'test_1' in file_contents and 'qwerty_1' in file_contents
 
 
-
 def test_init():
-    logger.init()
+    logger.init({'console': 'warning'})
     assert len(logger.HANDLERS) == 1
     assert isinstance(logger.HANDLERS[0], logging.StreamHandler)
-    logger.init({'file': 'debug'})
+    logger.init()
     assert len(logger.HANDLERS) == 2
-    assert isinstance(logger.HANDLERS[1], logging.FileHandler)
+    assert isinstance(logger.HANDLERS[0], logging.StreamHandler)
+    assert isinstance(logger.HANDLERS[1], logging.StreamHandler)
+    logger.init({'file': 'debug'})
+    assert len(logger.HANDLERS) == 3
+    assert isinstance(logger.HANDLERS[2], logging.FileHandler)
 
 
 def test__get_logger_type():
