515a8bb138dacabc68efc28365b97875e842b10b
Andrei Kuminov <akuminov@gmail.com>
2022-12-10 17:11:46 +1000

    plot in png

--- tests/view/plot_test.py
+++ tests/view/plot_test.py
@@ -1,3 +1,5 @@
+import io
+
 import pytest
 from view import plot
 
@@ -5,22 +7,41 @@ from view import plot
 import pandas as pd
 
 
-def test__get_graph_html():
+@pytest.fixture
+def df():
     df = pd.DataFrame.from_dict({
-        '2022-11-17 04:10:00': {'open':  0.00488, 'high':  0.00488, 'low':    0.00486, 'close':    0.00487, 'MAL':  0.004954, 'MAS':  0.004974},
-        '2022-11-17 04:15:00': {'open':  0.00487, 'high':  0.00489, 'low':  0.00486, 'close':    0.00489, 'MAL': 0.004953, 'MAS':  0.004966},
-        '2022-11-17 04:20:00': {'open':  0.00489, 'high':  0.00490, 'low':  0.00488, 'close':    0.00489, 'MAL':   0.004952, 'MAS':  0.004959},
-        '2022-11-17 04:25:00': {'open':  0.00489, 'high':  0.00490, 'low':  0.00488, 'close':    0.00490, 'MAL': 0.004952, 'MAS':  0.004953},
-        '2022-11-17 04:30:00': {'open':  0.00490, 'high':  0.00494, 'low':  0.00489, 'close':    0.00494, 'MAL':   0.004952, 'MAS':  0.004952},
-        '2022-11-17 04:35:00': {'open':  0.00494, 'high':  0.00557, 'low':  0.00494, 'close':    0.00543, 'MAL':   0.004956, 'MAS':  0.004997},
+        '2022-11-17 04:10:00': {'open':  0.00488, 'high':  0.00488, 'low':    0.00486, 'close':    0.00487, 'MAL':  0.004954, 'MAS':  0.004974, 'note': None},
+        '2022-11-17 04:15:00': {'open':  0.00487, 'high':  0.00489, 'low':  0.00486, 'close':    0.00489, 'MAL': 0.004953, 'MAS':  0.004966, 'note': None},
+        '2022-11-17 04:20:00': {'open':  0.00489, 'high':  0.00490, 'low':  0.00488, 'close':    0.00489, 'MAL':   0.004952, 'MAS':  0.004959, 'note': 'test'},
+        '2022-11-17 04:25:00': {'open':  0.00489, 'high':  0.00490, 'low':  0.00488, 'close':    0.00490, 'MAL': 0.004952, 'MAS':  0.004953, 'note': None},
+        '2022-11-17 04:30:00': {'open':  0.00490, 'high':  0.00494, 'low':  0.00489, 'close':    0.00494, 'MAL':   0.004952, 'MAS':  0.004952, 'note': 'signal'},
+        '2022-11-17 04:35:00': {'open':  0.00494, 'high':  0.00557, 'low':  0.00494, 'close':    0.00543, 'MAL':   0.004956, 'MAS':  0.004997, 'note': None},
         }, orient='index')
     df.index = pd.to_datetime(df.index)
-    df['ts'] = df.index.astype('int64')/1000000
+    df['ts'] = df.index.astype('int64') / 1000000
+    return df
+
+
+params = {
+    'lines': ['MAL', 'MAS'],
+    'notes': ['note']
+}
+
+def test__get_graph_png(df):
+    png = plot.plot_png(df, 'STMXUSDT', '5m', params)
+    assert isinstance(png, io.BytesIO)
+    with open('./test.png', 'wb') as f:
+        f.write(png.getbuffer())
+
+
+def test__get_graph_html(df):
 
     params = {'root_time': 1668659100325, 'top_time': 1668659700123,
             'kontr_min_time': 1668658200020, 'kontr_max_time':   1668658800400}
-    df(pd.to_datetime('2022-11-17 04:20:00').to_datetime64().astype('int64')/1000000)
+    print(pd.to_datetime('2022-11-17 04:20:00').to_datetime64().astype('int64')/1000000)
     # print(pd.to_datetime(scan['root_time']).floor('S'))
     html = plot.plot_html(df, 'STMXUSDT', '5m', params)
     # TODO test html
     # TODO implelemen symbols, plot lines and soon settled by params
+
+
--- view/plot.py
+++ view/plot.py
@@ -3,8 +3,43 @@ from bokeh.plotting import figure, show
 from bokeh.resources import CDN
 from bokeh.embed import file_html
 from bokeh.plotting import output_file, save
+import io
+import numpy as np
+import matplotlib.pyplot as plt
+import mplfinance as mpf
+import matplotlib.dates as mpl_dates
+
+plt.style.use('ggplot')
 
 
+
+def plot_png(df_graph: pd.DataFrame, symbol, tf='1s', params=None):
+    columns_names = ['Open', 'Close', 'Low', 'High', 'Volume']
+    df = df_graph.copy()
+    columns_rename = {col: col[0].upper() + col[1:].lower() for col in df.columns if col not in columns_names and col[0].upper() + col[1:].lower() in columns_names}
+    df.rename(columns=columns_rename, inplace=True)
+    df.index.name = 'Date'
+    if isinstance(params, dict):
+        apds = []
+        if isinstance(params.get('lines'), list):
+            apds.append(mpf.make_addplot(df[params['lines']]))
+        if isinstance(params.get('notes'), list):
+            notes_col = []
+            for col in params.get('notes'):
+                notes_col.append(f'{col}_YVAL')
+                df[f'{col}_YVAL'] = df[col].apply(lambda x: 1 if x else np.nan)
+                df[f'{col}_YVAL'] = df[f'{col}_YVAL'] * df['Low'] * 0.95
+                # df[f'{col}_YVAL'] = int(df[col]) * df['Low'] * 0.95
+                # df[f'{col}_YVAL'].replace(0, np.nan, inplace=True)
+            apds.append(mpf.make_addplot(df[notes_col], type='scatter', markersize=200, marker='^'))
+    else:
+        apds = None
+    df = df.fillna(value=np.nan)
+    buf = io.BytesIO()
+    mpf.plot(df, addplot=apds, type='candle', volume=False, savefig=buf)
+    buf.seek(0)
+    # IPydisplay.Image(buf.read())
+    return buf
 
 def plot_html(df_graph, symbol, tf='1s', params=None):
     html = None
